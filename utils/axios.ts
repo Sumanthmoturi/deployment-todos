dae4875143bc74aaea6161d4e3c85feaa3fc6229
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-16 21:23:59 +0530

    commit

--- utils/axios.ts
+++ utils/axios.ts
@@ -6,4 +6,14 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // Get token from localStorage
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`; // Attach token as Bearer token
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default instance;
