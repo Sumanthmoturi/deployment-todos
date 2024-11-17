62f5314b64d58f5d3081690900d76d9a32256d2c
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-17 19:46:14 +0530

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
+    
     <div className={styles.container}>
       <div className={styles.sticker}></div>
       <h1 className={styles.heading}>Welcome to the Todo App</h1>
