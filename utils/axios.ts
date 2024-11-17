10b1a0cdd7fbff625ebc18daed54c51fad63e71d
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-17 19:12:26 +0530

    commit

--- utils/axios.ts
+++ utils/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://deployment-todo-backend.onrender.com', 
-  withCredentials: true,
+  withCredentials: false,
 });
 
 
