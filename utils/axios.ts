e7d9c536637dd7b5d68b88f72512647d774cd4cb
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-18 11:25:02 +0530

    commit

--- utils/axios.ts
+++ utils/axios.ts
@@ -3,18 +3,13 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://deployment-todo-backend.onrender.com', 
-  withCredentials: true,
+  withCredentials: false, 
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // Get token from localStorage
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`; // Attach token as Bearer token
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+
 
 export default instance;
