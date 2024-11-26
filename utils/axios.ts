5339bb0f2870537919a17713e8a26b4c2f362c1c
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-26 14:00:24 +0530

    commit

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://ec2-13-127-145-5.ap-south-1.compute.amazonaws.com:443', 
+  baseURL: 'https://ec2-13-127-145-5.ap-south-1.compute.amazonaws.com:443', 
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
