09175e1322c5f4bbbbf686156d2e79991bfd5008
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-28 16:10:42 +0530

    deploy

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,15 +1,11 @@
-
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://deployment-todo-backend.onrender.com',
+  baseURL: 'https://13.201.224.43',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-
-
 
 export default instance;
