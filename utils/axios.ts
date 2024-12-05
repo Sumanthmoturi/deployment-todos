7b1f699ea2ec317d6b385a550dd6e3b2829df42f
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-05 15:21:11 +0530

    deploy

--- utils/axios.ts
+++ utils/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
 
 
-  baseURL: 'http://ec2-3-110-104-147.ap-south-1.compute.amazonaws.com:10000', 
+  baseURL: 'http://ec2-13-201-101-208.ap-south-1.compute.amazonaws.com:10000', 
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
