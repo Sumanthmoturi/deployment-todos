e7ff4f982736a2d51a131342b2bd06629aa93036
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-09 12:34:54 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-     
-      NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production'
-        ? 'https://deployment-todo-backend.onrender.com'  // Production API URL
-        : 'http://localhost:3001', 
-    },
-  };
+  reactStrictMode: true,
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 80,
+  },
+
   
-  export default nextConfig;
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production'
+      ? 'http://ec2-13-201-101-208.ap-south-1.compute.amazonaws.com'
+      : 'http://localhost:3001', 
+  },
   
+};
+
+export default nextConfig;
