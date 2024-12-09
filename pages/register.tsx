b3bf2ddd45ada5824aea57560eb4f2b7f95fd6ee
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-09 11:11:02 +0530

    commit

--- pages/register.tsx
+++ pages/register.tsx
@@ -39,7 +39,13 @@ type FormData = {
 };
 
 export default function Register() {
-  const { register, handleSubmit,clearErrors, formState: { errors }, setValue, setError } = useForm<FormData>();
+  const { register, handleSubmit,clearErrors, formState: { errors }, setValue, setError } = useForm<FormData>({
+    defaultValues: {
+      hobbies:[],
+      otherCountry:'',
+      otherHobby:'',
+    }
+  });
   const router = useRouter();
 
   const [showOtherCountry, setShowOtherCountry] = useState(false);
